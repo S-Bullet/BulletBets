6c710c3987800467793412a35bdcf6391ce15161
Tookssi <pjubullet816@gmail.com>
2024-02-16 17:59:32 +0900

    Record Payment Command(Premium Member)

--- src/bot_step3.js
+++ src/bot_step3.js
@@ -3,24 +3,54 @@ require('dotenv').config({ path: './../.env' });
 
 const PREFIX = 'pb!';
 const BOT_OWNER_ID = process.env.BOT_OWNER_ID;
+const PREMIUM_CUTOFF = 10;
 
 const bot = new eris.Client(process.env.TOKEN);
 
-const commandForName = {};
-commandForName['addpayment'] = {
-  botOwnerOnly: true,
-  execute: (msg, args) => {
-      const mention = args[0];
-      const amount = parseFloat(args[1]);
-
-      // TODO: Handle invalid command arguments, such as:
-      // 1. No mention or invalid mention.
-      // 2. No amount or invalid amount.
-
-      return msg.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`);
-  },
-};
-
+const premiumRole = {
+    name: 'Premium Member',
+    color: 0x6aa84f,
+    hoist: true, // Show users with this role in their own section of the member list.
+ };
+ 
+ async function updateMemberRoleForDonation(guild, member, donationAmount) {
+    // If the user donated more than $10, give them the premium role.
+    if (guild && member && donationAmount >= PREMIUM_CUTOFF) {
+        // Get the role, or if it doesn't exist, create it.
+        let role = Array.from(guild.roles.values())
+            .find(role => role.name === premiumRole.name);
+ 
+        if (!role) {
+            role = await guild.createRole(premiumRole);
+        }
+ 
+        // Add the role to the user, along with an explanation
+        // for the guild log (the "audit log").
+        return member.addRole(role.id, 'Donated $10 or more.');
+    }
+ }
+ 
+ const commandForName = {};
+ commandForName['addpayment'] = {
+    botOwnerOnly: true,
+    execute: (msg, args) => {
+        const mention = args[0];
+        const amount = parseFloat(args[1]);
+        const guild = msg.channel.guild;
+        const userId = mention.replace(/<@(.*?)>/, (match, group1) => group1);
+        const member = guild.members.get(userId);
+ 
+        // TODO: Handle invalid command arguments, such as:
+        // 1. No mention or invalid mention.
+        // 2. No amount or invalid amount.
+ 
+        return Promise.all([
+            msg.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`),
+            updateMemberRoleForDonation(guild, member, amount),
+        ]);
+    },
+ };
+ 
 bot.on('messageCreate', async (msg) => {
   try {
       const content = msg.content;
